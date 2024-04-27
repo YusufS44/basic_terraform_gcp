ae8313974ad1ab7d296d736789f5e909fa4b63ba
Yusufs44 <yusuf.sadruddin@gmail.com>
2024-04-26 23:22:59 -0400

    first commit

--- main.tf
+++ main.tf
@@ -2,55 +2,50 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "5.25.0"
+      version = "5.26.0"
     }
   }
 }
 
 provider "google" {
   # Configuration options
-  project = "gcp-class-417400"
   region = "us-east1"
-  zone = "us-east1-b"
+  project = "gcp-class-417400"
+  zone = "us-east1-a"
+  credentials = "gcp-class-417400-9aae2903fb6d.json"
+
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "tf-vpc-rdt-chewy"
-  auto_create_subnetworks = "false"  # Set to true to create a subnetwork automatically
+resource "google_storage_bucket" "bucket1" {
+  name          = "git_hub_file_update"
+  location      = "us-east1"
+  force_destroy = true
+
 }
 
-resource "google_compute_subnetwork" "subnetwork" {
-  name          = "terraform-subnetwork"
-  ip_cidr_range = "10.190.1.0/24"
-  region        = "us-east1"
-  network       = google_compute_network.vpc_network.id
+
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
+  auto_create_subnetworks = false
 }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-medium"
-  zone         = "us-east1-b"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-
-  network_interface {
-    network    = google_compute_network.vpc_network.id
-    subnetwork = google_compute_subnetwork.subnetwork.id
-
-    access_config {
-      // This line is necessary for the instance to have an external IP address
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
+resource "google_compute_subnetwork" "sub-sg" {
+  name ="sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.238.1.0/24"
+  region = "us-east1"
 }
+
+
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
+
+output "auto" {
+  value = google_compute_network.auto-vpc-tf.id
+}
+
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
